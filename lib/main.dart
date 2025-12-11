bb892b411bec29cafe62e4da3c2fa8f80cdc5ff8
Tauhid Musa Rieon <tauhidmusarieon01@gmail.com>
2025-12-11 16:38:23 +0600

    Initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Furniture',
+      title: 'Furniture app',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         useMaterial3: true,
